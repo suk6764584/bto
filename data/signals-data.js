da766de6688ecd502bb4ff6d3d33ac54bdecb4ac
suk6764584 <rdx840021@gmail.com>
2026-08-26 14:44:23 +0900

    Add event and cruise signal data

---
+++ data/signals-data.js
@@ -0,0 +1,23 @@
+window.APP_DATA = window.APP_DATA || {};
+window.APP_DATA.festivalEvents = [
+  {name:'2026 부산돼지국밥대전',start:'2026-08-16',end:'2026-10-18',district:'all',source:'https://www.visitbusan.net/kr/schedule/list.do?boardId=BBS_0000009&menuCd=DOM_000000204012000000&paging=ok&startPage=1'},
+  {name:'2026 별바다부산 나이트 뮤직 캠크닉',start:'2026-08-10',end:'2026-09-19',district:'all',source:'https://www.visitbusan.net/kr/schedule/list.do?boardId=BBS_0000009&menuCd=DOM_000000204012000000&paging=ok&startPage=1'},
+  {name:'해녀와 함께 바다를 배우다',start:'2026-08-04',end:'2026-09-19',district:'all',source:'https://www.visitbusan.net/kr/schedule/list.do?boardId=BBS_0000009&menuCd=DOM_000000204012000000&paging=ok&startPage=1'},
+  {name:'2026 별바다부산 나이트페스타',start:'2026-08-01',end:'2026-12-31',district:'all',source:'https://www.visitbusan.net/kr/schedule/list.do?boardId=BBS_0000009&menuCd=DOM_000000204012000000&paging=ok&startPage=1'},
+  {name:'광안리 M 드론라이트쇼 8월 프로그램',start:'2026-08-08',end:'2026-08-29',district:'수영구',source:'https://www.visitbusan.net/kr/schedule/list.do?boardId=BBS_0000009&menuCd=DOM_000000204012000000&paging=ok&startPage=1'},
+  {name:'2026 부산 나이트 워크 42K',start:'2026-08-29',end:'2026-08-30',district:'all',source:'https://www.visitbusan.net/kr/schedule/list.do?boardId=BBS_0000009&menuCd=DOM_000000204012000000&paging=ok&startPage=1'},
+  {name:'2026부산비엔날레',start:'2026-08-29',end:'2026-11-01',district:'all',source:'https://www.visitbusan.net/kr/schedule/list.do?boardId=BBS_0000009&menuCd=DOM_000000204012000000&paging=ok&startPage=1'}
+];
+window.APP_DATA.cruiseEvents = [
+  {name:'AIDAdiva 신규 입항',date:'2026-01-12',note:'약 2천여 명 승객 입항 보도',source:'https://www.busan.go.kr/nbtnewsBU/1714323'},
+  {name:'Regatta 오버나잇 크루즈',date:'2026-02-23',note:'1박 2일 체류형 크루즈 보도',source:'https://www.busanpa.com/board/view.do?boardId=BBS_0000031&dataSid=34830&menuCd=DOM_000000105002001000'},
+  {name:'Le Soleal 모항 크루즈',date:'2026-03-24',note:'항공·철도 연계 모항 운영 개시',source:'https://www.busanpa.com/board/view.do?boardId=BBS_0000031&dataSid=36064&menuCd=DOM_000000105002001000'}
+];
+window.APP_DATA.dataLinks = {
+  bus:'https://www.data.go.kr/data/15092750/openapi.do',
+  bims:'https://bus.busan.go.kr/busanBIMS/mobile/webApp/page/main/main.asp',
+  tour:'https://www.data.go.kr/tcs/dss/selectApiDataDetailView.do?publicDataPk=15101578',
+  events:'https://www.visitbusan.net/kr/schedule/list.do?boardId=BBS_0000009&menuCd=DOM_000000204012000000&paging=ok&startPage=1',
+  cruise:'https://www.chainportal.co.kr/mobile/nexacro/index.html?params=cruise&screenid=screen_mobile',
+  cruiseContext:'https://busanpa.com/board/view.bpa?boardId=BBS_0000031&dataSid=34722&menuCd=DOM_000000105002001000&paging=ok&startPage=12'
+};
